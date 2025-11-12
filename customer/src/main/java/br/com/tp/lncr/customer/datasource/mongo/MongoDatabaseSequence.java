9dfc30d10f9dbbd532795c4bd7db852654ba05a2
titoparizotto <titoparizotto@gmail.com>
2025-11-12 20:11:00 -0300

    fix: erro no teste com MongoDatabaseSequence.java e descomentado analise do sonar.

--- customer/src/main/java/br/com/tp/lncr/customer/datasource/mongo/MongoDatabaseSequence.java
+++ customer/src/main/java/br/com/tp/lncr/customer/datasource/mongo/MongoDatabaseSequence.java
@@ -10,7 +10,7 @@ public class MongoDatabaseSequence {
     private String id;
     private Integer seq;
 
-    private MongoDatabaseSequence() {
+    public MongoDatabaseSequence() {
     }
 
     public String getId() {
